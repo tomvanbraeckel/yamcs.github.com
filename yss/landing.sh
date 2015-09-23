95e44cf898bcade6a381a5d5e607218134be6dde
Fabian Diet <fabian.diet@spaceapplications.com>
2015-09-23 10:45:22 +0200

    Modify yss configuration files

---
+++ yss/landing.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+BASE_URL="http://www.yamcs.org"
+
+set -e
+/bin/echo -n "Downloading YSS Landing configuration to `pwd`/landing..... "
+if [ ! -e landing ]; then
+    mkdir landing
+fi
+curl -sSL "$BASE_URL"/yss/landing/docker-compose.yml > landing/docker-compose.yml
+curl -sSL "$BASE_URL"/yss/landing/Dockerfile > landing/Dockerfile
+curl -sSL "$BASE_URL"/yss/landing/logging.yamcs-server.properties > landing/logging.yamcs-server.properties
+curl -sSL "$BASE_URL"/yss/landing/tcp.yaml > landing/tcp.yaml
+echo "[ done ]"
---
+++ yss/leo_spacecraft.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+BASE_URL="http://www.yamcs.org"
+
+set -e
+/bin/echo -n "Downloading YSS LEO Spacecraft configuration to `pwd`/leo_spacecraft..... "
+if [ ! -e leo_spacecraft ]; then
+    mkdir leo_spacecraft
+fi
+if [ ! -e leo_spacecraft/simulator ]; then
+    mkdir leo_spacecraft/simulator
+fi
+if [ ! -e leo_spacecraft/yamcs ]; then
+    mkdir leo_spacecraft/yamcs
+fi
+curl -sSL "$BASE_URL"/yss/leo_spacecraft/docker-compose.yml > leo_spacecraft/docker-compose.yml
+curl -sSL "$BASE_URL"/yss/leo_spacecraft/simulator/Dockerfile > leo_spacecraft/simulator/Dockerfile
+curl -sSL "$BASE_URL"/yss/leo_spacecraft/yamcs/Dockerfile > leo_spacecraft/yamcs/Dockerfile
+curl -sSL "$BASE_URL"/yss/leo_spacecraft/yamcs/logging.yamcs-server.properties > leo_spacecraft/yamcs/logging.yamcs-server.properties
+curl -sSL "$BASE_URL"/yss/leo_spacecraft/yamcs/tcp.yaml > leo_spacecraft/yamcs/tcp.yaml
+echo "[ done ]"
